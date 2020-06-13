406b0f96ada19877ef6cb3ce360216578c8ee5f1
Nicolas Ries <nries1@gmail.com>
2020-06-13 13:41:56 -0400

    created models

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 3001;
+const { startDb } = require('./utils');
 
 app.use(bodyParser.json());
 
@@ -12,6 +13,9 @@ app.get('/', (_request, response) => {
 
 app.use('/api', routes);
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+// Start DB before starting server
+startDb().then(() => {
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+  });
 });
