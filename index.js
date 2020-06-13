4b82ef6fbe24164e6a39c4cfe1abc1ea1af8c154
Xolo <solomonmanyaga@gmail.com>
2020-06-13 11:16:37 -0400

    db setup

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const Sequelize = require("sequelize");
 
 const app = express();
 
 
-let db = new Sequelize("database", "username", "password", {
-  host: "localhost",
-  dialect: "postgres",
+// let db = new Sequelize("database", "username", "password", {
+//   host: "localhost",
+//   dialect: "postgres",
 
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000,
+//   },
 
-});
+// });
 
 
 // DB test 
-db.Authenticate()
-    .then( () => console.log("connection working!!!"))
-    .catch(err => console.log(err))
+// db.Authenticate()
+//     .then( () => console.log("connection working!!!"))
+//     .catch(err => console.log(err))
 
-app.get("/", (req, res) => {
-    res.send("Redire¢t coming very very Soon!");
-});
+// app.get("/", (req, res) => {
+//     res.send("Redire¢t coming very very Soon!");
+// });
 
 const port = process.env.PORT || 5000;
 
