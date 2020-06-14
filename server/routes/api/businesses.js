11d4ce0925b99c63e0c7984174290b3dea42b50e
mjr68 <matthewrose1799@gmail.com>
2020-06-14 10:56:01 -0400

    deploy test

--- server/routes/api/businesses.js
+++ server/routes/api/businesses.js
@@ -1,28 +1,27 @@
 //TODO: any data validation would happen within the routes
-
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const Business = require('../../../db/models/businesses');
 
-router.get("/test", (req, res) => {
-    res.json({ msg: "Welcome to businessess" });
+router.get('/test', (req, res) => {
+  res.json({ msg: 'Welcome to businessess' });
 });
 
-router.get("/test2", (req, res) => {
-    res.json({ msg: "Another route" });
+router.get('/test2', (req, res) => {
+  res.json({ msg: 'Another route' });
 });
 
-router.get("/getAll", (req, res) => {
-    // res.json({ msg: "All businesses" });
-    Business.findAll()
-        .then((Businesses) => res.json(Businesses))
-        .catch((err) =>
-            res
-            .status(404)
-            .json({ noBusinessesFound: "No Businesses foundin DB" })
+router.get('/getAll', (req, res) => {
+  // res.json({ msg: "All businesses" });
+  Business.findAll()
+    .then((Businesses) => res.json(Businesses))
+    .catch(
+      (err) => console.log(err)
+      // res
+      // .status(404)
+      // .json({ noBusinessesFound: "No Businesses foundin DB" })
     );
 });
-
 
 // router.get("/getBusiness/:id", (req, res) => {});
 // router.get("/addBusiness", (req, res) => {});
